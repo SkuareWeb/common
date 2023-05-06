7ce31954852a3ee5f795f316997b95fd35ac40ec
Zubair Shaikh <zsescape@gmail.com>
2023-05-06 13:36:40 -0400

    Update IExploreResult

--- src/interfaces/ExploreResult.ts
+++ src/interfaces/ExploreResult.ts
@@ -1,9 +1,11 @@
 import { IArea } from "./Area";
 import { IArtist } from "./Artist";
+import { IEvent } from "./Event";
 import { IEventType } from "./EventType";
 
 export interface IExploreResult {
   city: string;
+  featured: IEvent[];
   areas: IArea[];
   artists: IArtist[];
   eventTypes: IEventType[];
