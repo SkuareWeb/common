40368cf1d19bd9a866b8657853e696f6fa093ffe
Zubair Shaikh <zsescape@gmail.com>
2023-06-25 13:13:26 -0400

    Update AppUser - Remove favorites from AppUser

--- dist/interfaces/AppUser.d.ts
+++ dist/interfaces/AppUser.d.ts
@@ -1,10 +1,8 @@
 import { IUser } from "./User";
 import { UserType } from "../constants";
 import { ICity } from "./City";
-import { IEvent } from "./Event";
 export interface IAppUser extends IUser {
     type: UserType.AppUser;
     city?: string | ICity;
     tnc: boolean;
-    favourites: Array<string | IEvent>;
 }
