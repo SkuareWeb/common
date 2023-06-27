4a2593883ec8f65f7405bce4e9be672851257eea
Zubair Shaikh <zsescape@gmail.com>
2023-06-27 10:42:44 -0400

    Update Cuisine - Add isActive - Add createdAt - Add updatedAt

--- src/interfaces/Cuisine.ts
+++ src/interfaces/Cuisine.ts
@@ -3,6 +3,9 @@ import { IHistoryBase } from "./HistoryBase";
 export interface ICuisine {
   id: string;
   name: string;
+  isActive: boolean;
+  createdAt: string | Date;
+  updatedAt: string | Date;
 }
 
 export type ICuisineHistory = IHistoryBase<ICuisine>;
