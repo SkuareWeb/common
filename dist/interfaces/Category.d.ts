174c8a6a5b3e131b1e34ded9401507905eef833e
Zubair Shaikh <zsescape@gmail.com>
2023-06-27 10:42:29 -0400

    Update Category - Add isActive - Add createdAt - Add updatedAt

--- dist/interfaces/Category.d.ts
+++ dist/interfaces/Category.d.ts
@@ -2,5 +2,8 @@ import { IHistoryBase } from "./HistoryBase";
 export interface ICategory {
     id: string;
     name: string;
+    isActive: boolean;
+    createdAt: string | Date;
+    updatedAt: string | Date;
 }
 export type ICategoryHistory = IHistoryBase<ICategory>;
--- src/interfaces/Category.ts
+++ src/interfaces/Category.ts
@@ -3,6 +3,9 @@ import { IHistoryBase } from "./HistoryBase";
 export interface ICategory {
   id: string;
   name: string;
+  isActive: boolean;
+  createdAt: string | Date;
+  updatedAt: string | Date;
 }
 
 export type ICategoryHistory = IHistoryBase<ICategory>;
