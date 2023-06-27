9b3c2b648720901819e9bbd720f6e874d8ab7a07
Zubair Shaikh <zsescape@gmail.com>
2023-06-27 10:59:09 -0400

    Update Category - Add description

--- dist/interfaces/Category.d.ts
+++ dist/interfaces/Category.d.ts
@@ -2,6 +2,7 @@ import { IHistoryBase } from "./HistoryBase";
 export interface ICategory {
     id: string;
     name: string;
+    description: string;
     isActive: boolean;
     createdAt: string | Date;
     updatedAt: string | Date;
--- src/interfaces/Category.ts
+++ src/interfaces/Category.ts
@@ -3,6 +3,7 @@ import { IHistoryBase } from "./HistoryBase";
 export interface ICategory {
   id: string;
   name: string;
+  description: string;
   isActive: boolean;
   createdAt: string | Date;
   updatedAt: string | Date;
